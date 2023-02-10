761805ac08c8d0db5cba516daf49426e06fd99be
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-10 01:17:49 -0600

    Variables-Primitive data types

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int h = 9;
+        System.out.println(h);
+        int s;
+        s = 7;
+        System.out.println(s);
+        long myVariable = 18L;
+        long anotherVariableWhichIsLong = 3000000000L;
+        double myDouble = 18.3;
+        float myFloat = 18.3F;
+    }
+}
