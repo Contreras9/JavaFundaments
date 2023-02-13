a868c659e21473d3b54ea5328df9db1c3113657f
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-13 13:53:58 -0600

    Code Structure 1-Using methods

--- src/Methods.java
+++ src/Methods.java
@@ -4,5 +4,41 @@ public class Methods {
         for (String arg : args) {
             System.out.println(arg);
         }
+
+        int result = addTwoNumbers(3, 6);
+        System.out.println("The result was " + result + ".");
+    }
+
+    public static int whoIsOlder(int age1, int age2) {
+        if (age1 > age2) {
+            return age1;
+        }
+        return age2;
+    }
+
+    public static boolean longerThan5(String word) {
+//        if (word.length() >= 6) {
+//            return true;
+//        }
+//        return false;
+
+//        return word.length() >= 6 ? true : false;
+
+        return word.length() >=6;
+    }
+
+    public static int addTwoNumbers(int num1, int num2) {
+        System.out.println(num1 + num2);
+        printMyName();
+        if (num1 == 0) {
+            return -1;
+        }
+        else {
+            return num1 + num2;
+        }
+    }
+
+    public static void printMyName() {
+        System.out.println("My name is Hamzah.");
     }
 }
