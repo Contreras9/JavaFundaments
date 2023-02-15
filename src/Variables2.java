46c3e14e4d4a36dd2dc69c6e181f46057ecbc670
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-14 22:47:54 -0600

    Working with classes-Wrapper classes for primatives

---
+++ src/Variables2.java
@@ -0,0 +1,12 @@
+public class Variables2 {
+
+    public static void main(String[] args) {
+        int i = 9;
+        double d = 9.3;
+
+        Integer i2 = 9;
+        Double d2 = 9.3;
+
+        // Double d2 = new Double(9.3);
+    }
+}
