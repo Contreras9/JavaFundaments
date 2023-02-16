9a8710ae609dac99b37bb1571913799111bfa87a
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-15 23:06:18 -0600

    Arrays Of Objects-The meaning of null

--- src/ArraysOfObjects.java
+++ src/ArraysOfObjects.java
@@ -7,5 +7,15 @@ public class ArraysOfObjects {
         Book[] books = new Book[10];
 
         books[4] = new Book("A good read", "Matt Greencroft", 17.99);
+
+        Book myBook;
+        myBook = new Book("A good read part 2", "Matt Greencroft", 37.99);
+//        myBook = null;
+
+        if (myBook != null) {
+            System.out.println(myBook.getTitle());
+        }
+
+        books[5] = myBook;
     }
 }
