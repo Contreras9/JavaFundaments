c22f19f5db0833ebbc678f9061146d4370dcd946
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-22 00:10:00 -0600

    Time & Money-Dates & Times

--- src/JavaLibraries.java
+++ src/JavaLibraries.java
@@ -1,12 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class JavaLibraries {
 
     public static void main(String[] args) {
         Date today = new Date();
-        int hours = today.getHours();
-        System.out.println(today);
-        System.out.println(hours);
+//        int hours = today.getHours();
+//        System.out.println(today);
+//        System.out.println(hours);
 
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(today);
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) - 1);
+        System.out.println(calendar.getTime());
+
+        GregorianCalendar birthday = new GregorianCalendar(1936, 3, 16);
+        Date birthdayDate = birthday.getTime();
+
+        System.out.println(today);
+        System.out.println(calendar);
+        System.out.println(birthday);
+        System.out.println(birthdayDate);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String prettyDate = sdf.format(birthdayDate);
+        System.out.println(prettyDate);
     }
 }
