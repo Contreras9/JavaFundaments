bd9ee97cdab9b07b8f22b531d6b87e9a294a9235
Hamzah Contreras <h.contreras9@outlook.com>
2023-02-22 22:27:12 -0600

    Time & Money-Working with dates and times

--- src/JavaLibraries.java
+++ src/JavaLibraries.java
@@ -1,4 +1,9 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -27,5 +32,19 @@ public class JavaLibraries {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String prettyDate = sdf.format(birthdayDate);
         System.out.println(prettyDate);
+
+        LocalDateTime theDateAndTime = LocalDateTime.now();
+        LocalDateTime anotherDateAndTime = LocalDateTime.of(2023, 9, 18, 17 , 15);
+        LocalDate someDate = LocalDate.of(2023, 6, 11);
+        LocalTime someTime = LocalTime.from(anotherDateAndTime);
+
+        System.out.println(theDateAndTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")));
+        System.out.println(someTime);
+
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        LocalDate myBirthday = LocalDate.of(1990, 5, 27);
+        Period difference = Period.between(myBirthday, yesterday);
+        System.out.println("There are " + difference.getYears() + " years, " + difference.getMonths() + " months, and " + difference.getDays() + " days between the two dates.");
+
     }
 }
