3a7bf264c8bf98b52a4e9d78fe4dd878e59a824e
Hamzah Contreras <h.contreras9@outlook.com>
2023-03-02 23:48:42 -0600

    Exceptions-Checked and unchecked exceptions

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,3 +1,5 @@
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 
 public class Exceptions {
@@ -5,7 +7,28 @@ public class Exceptions {
     public static void dividing(int denominator) {
         System.out.println(17 / denominator);
     }
-    public static void main(String[] args) {
+
+    public static void createURI() {
+        try {
+            URI uri = new URI("-https://www.google.com");
+            System.out.println("The URI was created.");
+        }
+        catch (URISyntaxException e) {
+            throw new IllegalArgumentException();
+        }
+        catch (Exception e) {
+            System.out.println("The exception was caught!");
+        }
+        finally {
+            System.out.println("This is in the finally block!");
+        }
+        System.out.println("This code is after try-catch.");
+    }
+
+    public static void createURIv2() throws URISyntaxException {
+        URI uri = new URI("-https://www.google.com");
+    }
+    public static void main(String[] args) throws URISyntaxException {
 
 //        Integer[] integerArray = new Integer[] {1,2,3,4,5};
 //        System.out.println(integerArray[5]);
@@ -23,5 +46,11 @@ public class Exceptions {
 
         User user1 = new User("Matt", 21);
         User user2 = new User("Sally", 43);
+
+        createURI();
+        createURIv2();
+
+        System.out.println("This is still printing.");
+
     }
 }
